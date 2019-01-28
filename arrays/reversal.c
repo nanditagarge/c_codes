e8aa375089b5e5ca15a24a9ac1ce4f8c3e890a29
nanditagarge <b118028@iiit-bh.ac.in>
2019-01-28 08:47:57 -0500

    reversal

---
+++ arrays/reversal.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int a[100],i,n,x;
+	printf("enter the number of elements in the array\n");
+	scanf("%d",&n);
+	printf("enter the elements of the array\n");
+	for(i=0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<=n/2-1;i++){
+		x=a[i];
+		a[i]=a[n-i-1];
+		a[n-i-1]=x;
+	}
+	printf("the new reversed array is:\n");
+	for(i=0;i<n;i++){
+		printf("%d",a[i]);
+	}
+	return 0;
+}
