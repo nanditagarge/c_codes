88fe0905a2deac4e1122798ec1bbbe42df234525
sgarge <shreya.garge@oracle.com>
2019-02-06 20:26:22 +0000

    fixing insertion at the end of linked list

--- arrays/inend.c
+++ arrays/inend.c
@@ -6,82 +6,102 @@ struct node
 	struct node* link;
 
 }* start;
-void create_list(int data);
-void insertend(int x);
-void display();
-void main()
+
+
+void create_list(int data[], int n){
+
+	struct node *q, *temp;
+	start = (struct node *)malloc(sizeof(struct node));
+
+	if(start == NULL){
+		printf("unable to allocate memory! bad bad computer!");
+	}
+
+	else{
+		start -> info = data[0];
+		start -> link = NULL;
+
+		temp = start;
+	
+		for(int i=1; i<n; i++){
+			q = (struct node *)malloc(sizeof(struct node));
+			if(q == NULL){
+				printf("Unable to allocate memory.");
+				break;
+			}
+
+			else{
+				q -> info = data[i];
+				q -> link = NULL;
+
+				temp -> link = q;
+				temp = temp -> link;
+			}
+		}
+	}
+}
+
+void insertend(int x){
+
+	struct node *temp, *new;
+	new = (struct node*)malloc(sizeof(struct node));
+
+	if(new == NULL){
+		printf("unable to allocate memory.\n");
+	}
+	else{
+		new -> info = x;
+		new -> link = NULL;
+		if(start == NULL) start=new;
+		else{
+			temp = start;
+			while(temp -> link != NULL){
+				temp = temp -> link;
+			}
+			temp -> link = new;		
+		}
+	}
+}
+
+void display(){
+
+	struct node *q;
+	if(start == NULL){
+		printf("Boo! list is empty :(");
+	}
+
+	else{
+		q = start;
+		while(q!=NULL){
+			printf("%d\t",q-> info);
+			q = q -> link;
+		}
+	}
+	printf(".\n");
+}
+
+int main()
 {
-	int i,n,data,x;
-	start==NULL;
+	int i,n,x;
+	start=NULL;
 	printf("enter the number of nodes\n");
 	scanf("%d",&n);
+	int data[n];
 	for(i=0;i<n;i++){
 		printf("enter data into %d node\n",i);
-		scanf("%d",&data);
-		create_list(data);
+		scanf("%d",&data[i]);
 	}
+	create_list(data,n);
+	printf("the list is created thusly :P\n");
+	display();
 
 	printf("enter the value of data to be entered into the new node\n");
 	scanf("%d",&x);
 	
 	insertend(x);
+	printf("the element has been inserted at the end thusly! wohoo! :D\n");
+	
 	display();
-
-
-}
-void create_list(int data)
-{
-	struct node* q,* temp;
-	temp=malloc(sizeof(struct node));
-	temp -> info = data;
-	temp -> link = NULL;
-	if(start==NULL)
-		start=temp;
-	else
-	{
-		q=start;
-		while(q!=NULL)
-			q =q -> link;
-		q -> link = temp;
-	}
-
-
-}
-void insertend(int x)
-{
-	struct node* q,* new;
-	new=malloc(sizeof(struct node));
-	new -> info = x;
-	new -> link = NULL;
-	if(start == NULL)
-		start=new;
-	else
-	{
-		q=start;
-		while(q -> link!= NULL)
-		{
-			q = q -> link;
-
-		}
-		
-		q -> link=new;	
-			
-	}
-
-	
-}
-void display()
-{
-	struct node* q;
-	q = start;
-	while(q!=NULL)
-	{
-		printf("%d",q-> info);
-
-		q = q -> link;
-	}
-	
-
-	printf(".");
+	return 0;
 
 }
